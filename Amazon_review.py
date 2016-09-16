66dae1140572028d659062f192d4a3631ad99dc0
sarmstr5 <git.sarmstr5@gmail.com>
2016-09-16 00:38:07 -0400

    Added new selection features as well as distance calculations.  Objects are now compared by positive and negative sentiments and strength length.  Something worth looking at is the dot product difference.  Still need to figure out weighted distance voting, need to find k, cross validation, reading in dictionary, etc.

--- Amazon_review.py
+++ Amazon_review.py
@@ -2,12 +2,58 @@ class Amazon_review:
     global sentiment
     global text
     global mapping 
-    
+    _some_big_number = 100000000
+    _least_negative_review = _some_big_number 
+    _least_positive_review = _some_big_number
+    _most_positive_review = 0
+    _most_negative_review = 0
+    _longest_review = 0
+    _shortest_review = _some_big_number
+
     def __init__(self, text):
         self.text = text
-        sentiment = ''
-        mapping = 0 
+        self.sentiment = ''
+        self.mapping = 0 
 
-    
     def toPrint(self):
         print(sentiment, text, mapping)
+
+    #defining class properties for normalizing
+    @property
+    def least_negative_review(self):
+        return self._least_negative_review
+    @property
+    def least_positive_review(self):
+        return self._least_positive_review
+    @property
+    def most_negative_review(self):
+        return self._most_negative_review
+    @property
+    def most_positive_review(self):
+        return self._most_positive_review
+    @property
+    def longest_review(self):
+        return self._longest_review
+    @property
+    def shortest_review(self):
+        return self._shortest_review
+
+    #creating class property setters
+    @least_negative_review.setter
+    def least_negative_review(self, value):
+        self._least_negative_review = value
+    @least_positive_review.setter
+    def least_positive_review(self, value):
+        self._least_positive_review = value
+    @most_negative_review.setter
+    def most_negative_review(self, value):
+        self._most_negative_review = value
+    @most_positive_review.setter
+    def most_positive_review(self, value):
+        self._most_positive_review = value
+    @longest_review.setter
+    def longest_review(self, value):
+        self._longest_review = value
+    @shortest_review.setter
+    def shortest_review(self, value):
+        self._shortest_review = value
--- main.py
+++ main.py
@@ -3,22 +3,34 @@ from Amazon_review import Amazon_review
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import math
+import datetime
+from sys import stdout
+from time import sleep
 from nltk import corpus
 from nltk import tokenize as tok
 
 training_set = []
 test_set = []
-k = 4 #needs to be updated
+k = 5 #needs to be updated
 stop_words = set(corpus.stopwords.words("english"))
 stop_words.discard("not")
-train_data = 'sm_train.dat'
-test_data = 'sm_test.dat'
+small_train = 'sm_train.dat'
+small_test = 'sm_test.dat'
+train = 'train_data'
+test = 'test_data'
+train_data = train
+test_data = test
 sent_dict = {}
+freq_dict = {}
 #----for normalization---#
 longest_review = 0
+shortest_review = 0
 most_negative_review = 0
+least_negative_review = 0
 most_positive_review = 0
-
+least_positive_review = 0
+begin_time = datetime.datetime.now().time()
 #------------------------#
 #using sentiWordNet as dictionary for sentiment
 #no particular reason to use sentiWordNet over others
@@ -41,7 +53,8 @@ def read_in_dict(word_dictionary):
             except ValueError:
                 pass
             new_dict[first_term]= [float(row[2]), float(row[3])]
-        return new_dict
+
+    return new_dict
 
 
 def plot(test_results):
@@ -52,7 +65,7 @@ def plot(test_results):
     plt.show()
 
 def tokenizer(review):
-    print("------> In tokenizer\n")
+#    print("------> In tokenizer\n")
     tokenized_review = tok.word_tokenize(review)
     trimmed_review = []
     for w in tokenized_review:
@@ -82,46 +95,88 @@ def parse_test_set(test_file, sent_dict):
 
     return reviews
     
-#Need to update
 def value_calc(review, sentiment_dict):
+#    print("------> In value_calc \n")
     length = len(review.text)
+    global most_negative_review
+    global least_negative_review
+    global most_positive_review
+    global least_positive_review
+    global longest_review
+    global shortest_review
     pos_count = 0
     neg_count = 0
+
     for word in review.text:
         if word in sentiment_dict:
-            #freq_dict[word]+=1
+            #freq_dict[word] = freq_dict.get(word, 0)+1
             pos_count+= sentiment_dict[word][0]
             neg_count+= sentiment_dict[word][1]
 
-    if(most_negative_review<neg_count):
-        global most_negative_review = neg_count
-    if(most_positive_review<pos_count):
-        global most_positive_review = pos_count
-    if(longest_review<length):
-        global longest_review = length
-
-    return [pos_count, neg_count, length]
+    #keeping track of normalization variables
     
+    if(most_negative_review<neg_count):
+        most_negative_review = neg_count
+    elif(least_negative_review>neg_count):
+        least_negative_review = neg_count
+    if(most_positive_review<pos_count):
+        most_positive_review = pos_count
+    elif(least_negative_review>pos_count):
+        least_negative_review = pos_count
+    if(longest_review<length):
+        longest_review = length
+    elif(shortest_review>length):
+        shortest_review = length
+#    if(review.most_negative_review<neg_count):
+#        print(review.most_negative_review)
+#        review.most_negative_review=neg_count
+#        print(review.most_negative_review)
+#        print (most_negative_review)
+    return [pos_count, neg_count, length]
 
 def k_NN(k,training_set,test_set):
     #for each test entity z
     #select k nearest xi's
     #call set selection algorithm
     #add z to classification set
-    print("------> In K-NN")
+    #print("------> In K-NN")
     neighbors = []
-    output= open("sentiment_output",'wt')
+    output= open("sentiment_output"+str(datetime.datetime.now().time()),'wt')
     i =0
     #need to return the the tuples of the training set (at least the sentiment)
     for review in test_set:
         neighbors = nsmallest(k,training_set, key= lambda train_review:
-                              abs(train_review.mapping-review.mapping))
+                              get_dist(train_review, review))
         review.sentiment = vote(neighbors, review)
         #print(str(test_set.index(review)),review.sentiment)
         i+=1
-        print(i)
-        output.write(str(review.sentiment)+'\t'+str(review.mapping)+"\n")
+        #output.write(str(review.sentiment)+'\t'+str(review.mapping)+"\n")
+        output.write(str(review.sentiment)+"\n")
+        stdout.write("\r%d"%i)
+        stdout.flush()
+    stdout.write('\n')
     return test_set
+
+def normalize(x, xmin, xmax):
+    return((x - xmin)/(xmax - xmin))
+
+def get_dist(review1, review2):
+    global most_negative_review 
+    global least_negative_review
+    global most_positive_review
+    global least_positive_review
+    global longest_review
+    global shortest_review
+    r1_p = normalize(review1.mapping[0],least_positive_review,most_positive_review)
+    r1_n = normalize(review1.mapping[1],least_negative_review,most_negative_review)
+    r1_d = normalize(review1.mapping[2],shortest_review, longest_review)
+    r2_p = normalize(review2.mapping[0],least_positive_review,most_positive_review)
+    r2_n = normalize(review2.mapping[1],least_negative_review,most_negative_review)
+    r2_d = normalize(review2.mapping[2],shortest_review, longest_review)
+    #pythagorean theorem d = sqrt((x2-x1)^2+(y2-y1)^2)
+    distance = math.sqrt((r2_p-r1_p)**2 + (r2_n - r1_n)**2 + (r2_d - r1_d)**2)
+    #print(distance)
+    return distance
 
 def vote(k_neighbors, test_review):
     #print("------> In Vote")
@@ -129,9 +184,11 @@ def vote(k_neighbors, test_review):
     #can do majority wins or distance weighted
     positive_count = 0
     negative_count = 0
+    weighted_vote = 0
     for neighbor in k_neighbors:
         if(neighbor.sentiment == '+1'): #need to validate this check
             positive_count+=1
+            
         else:
             negative_count+=1
     if(positive_count>negative_count): 
@@ -145,9 +202,6 @@ def main():
     training_file = open(train_data,"r")
     test_file = open(test_data,"r")
     sent_dict = read_in_dict('senti')
-    most_negative_review = 0
-    most_positive_review = 0
-    longest_review = 0
     #-----------------------------------------------------#
 
     print("Initializing program")
@@ -165,6 +219,8 @@ def main():
     test_set = k_NN(k, training_list, test_list)
     training_file.close()
     test_file.close()
+    print(begin_time)
+    print(datetime.datetime.now().time())
     
 #    print("Training parse next feature calc")
 #    training_set = calc_feature(training_list)
