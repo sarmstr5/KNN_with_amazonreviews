564387f2216221bb04005ec1bac2f2541e69393b
sarmstr5 <git.sarmstr5@gmail.com>
2016-09-18 11:11:43 -0400

    formmating changes based from PyCharm

--- Create_training_dictionary.py
+++ Create_training_dictionary.py
@@ -1,4 +1,4 @@
-#Creating and writing the training dictionary for classifying
+# Creating and writing the training dictionary for classifying
 import csv
 import json
 import os
@@ -14,16 +14,17 @@ sent_dict = {}
 freq_dict = {}
 training_file = 'data/train_data'
 
-def tokenizer(review):
-#    print("------> In tokenizer\n")
-    tokenized_review = tok.word_tokenize(review)
+
+def tokenizer(token_review):
+    #    print("------> In tokenizer\n")
+    tokenized_review = tok.word_tokenize(token_review)
     trimmed_review = []
     for word in tokenized_review:
         if word not in stop_words:
             if '.' in word:
-                new_w=word.split('.')
+                new_w = word.split('.')
                 for i in new_w:
-                    if(i == ''):
+                    if (i == ''):
                         continue
                     trimmed_review.append(i.lower())
             else:
@@ -31,25 +32,26 @@ def tokenizer(review):
 
     return trimmed_review
 
+
 with open(training_file, 'r') as training_file:
     for line in training_file:
         split_line = line.split('\t', 1)
         review_sentiment = split_line[0]
-        #list of words without stop words
+        # list of words without stop words
         review = tokenizer(split_line[1])
         for word in review:
-            #if word doesnt exist in dictionary default value is 0
-            freq_dict[word] = freq_dict.get(word, 0) +1
-            if(review_sentiment == '+1'):
-                sent_dict[word] = sent_dict.get(word,0)+1
-            elif(review_sentiment == '-1'):
-                sent_dict[word] = sent_dict.get(word,0)-1 
+            # if word doesnt exist in dictionary default value is 0
+            freq_dict[word] = freq_dict.get(word, 0) + 1
+            if (review_sentiment == '+1'):
+                sent_dict[word] = sent_dict.get(word, 0) + 1
+            elif (review_sentiment == '-1'):
+                sent_dict[word] = sent_dict.get(word, 0) - 1
             else:
                 print("review didnt have sentiment???")
                 print(review)
 
-#read in each review of the training set
-#take out stop words
+# read in each review of the training set
+# take out stop words
 # add words from review to frequency dictionary as key value =+1
 # add words from review to sentiment dictionary as key, value =+ sentiment from review
 # only keep words with most sentiment and freq
@@ -59,23 +61,22 @@ trimmed_sent_dict = {}
 # dictionaries are hashtables so turning into tuples to sort by value
 print("length of frequency dictionary is " + str(len(freq_dict)))
 print("length of sentiment dictionary is " + str(len(sent_dict)))
-freq_sorted = sorted(freq_dict.items(), key = lambda x: x[1], reverse = True)
-sent_sorted = sorted(sent_dict.items(), key = lambda x: x[1])
-trimmed_freq_index = int(len(freq_sorted)*.3)
-trimmed_sent_index = int(len(sent_sorted)*.15)
-trimmed_sent_index_from_end = len(sent_sorted)-trimmed_sent_index
+freq_sorted = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
+sent_sorted = sorted(sent_dict.items(), key=lambda x: x[1])
+trimmed_freq_index = int(len(freq_sorted) * .3)
+trimmed_sent_index = int(len(sent_sorted) * .15)
+trimmed_sent_index_from_end = len(sent_sorted) - trimmed_sent_index
 
-#using the tuples to create trimmed dictionaries
+# using the tuples to create trimmed dictionaries
 for tup in freq_sorted[:trimmed_freq_index]:
     trimmed_freq_dict[tup[0]] = tup[1]
-for tup in sent_sorted[:trimmed_sent_index]: 
+for tup in sent_sorted[:trimmed_sent_index]:
     trimmed_sent_dict[tup[0]] = tup[1]
 for tup in sent_sorted[trimmed_sent_index_from_end:]:
     trimmed_sent_dict[tup[0]] = tup[1]
 
-#writing trimmed dictionaries to json files
+# writing trimmed dictionaries to json files
 with open('dictionaries/training_frequency_dict', 'w') as j_file:
-    json.dump(trimmed_freq_dict, j_file, indent = 4, sort_keys = True, separators=(',',': '))
+    json.dump(trimmed_freq_dict, j_file, indent=4, sort_keys=True, separators=(',', ': '))
 with open('dictionaries/training_sent_dict', 'w') as j_file:
-    json.dump(trimmed_sent_dict, j_file, indent = 4, sort_keys = True, separators=(',' , ': '))
-
+    json.dump(trimmed_sent_dict, j_file, indent=4, sort_keys=True, separators=(',', ': '))
--- Dictionary_cleanup.py
+++ Dictionary_cleanup.py
@@ -2,44 +2,46 @@ import csv
 import json
 import os
 
-#Clean up sentiWordNet dictionary and write as JSON object
-#using sentiWordNet as dictionary for sentiment
-#no particular reason to use sentiWordNet over others
-#senti has 6 columns (pos, id, PosScore, NegScore, SynsetTerms, Gloss)
-#the column SynsetTerms has same words with different endings
+# Clean up sentiWordNet dictionary and write as JSON object
+# using sentiWordNet as dictionary for sentiment
+# no particular reason to use sentiWordNet over others
+# senti has 6 columns (pos, id, PosScore, NegScore, SynsetTerms, Gloss)
+# the column SynsetTerms has same words with different endings
 
 current_directory = os.path.dirname(__file__)
 word_dictionary = 'dictionaries/senti'
 
+
 def strip_nums(s):
     return ''.join([i for i in s if not i.isdigit() or '']).strip()
 
+
 with open(word_dictionary) as csv_file:
-    dict_csv = csv.reader(csv_file,delimiter = '\t')
+    dict_csv = csv.reader(csv_file, delimiter='\t')
     new_dict = {}
     for row in dict_csv:
-        #File has empty lines so try to read if fail skip
-        #file has comments and terms that have 0 for positive and negative weights
-        #taking them out of the dict
-        if(row[0].startswith('#')):
+        # File has empty lines so try to read if fail skip
+        # file has comments and terms that have 0 for positive and negative weights
+        # taking them out of the dict
+        if (row[0].startswith('#')):
             continue
         try:
-            pos_score = float(row[2]) 
+            pos_score = float(row[2])
             neg_score = float(row[3])
             obj_score = (pos_score - neg_score)
             if obj_score == 0:
                 continue
-            #I have a string of terms with hashtags and nums denoting which term
+            # I have a string of terms with hashtags and nums denoting which term
             terms_no_nums = strip_nums(row[4]).strip()
             terms = terms_no_nums.split('#')
             for word in terms:
                 trimmed_word = word.strip()
-                if(trimmed_word == '' or '_' in trimmed_word):
+                if (trimmed_word == '' or '_' in trimmed_word):
                     continue
                 new_dict[trimmed_word] = obj_score
 
         except ValueError:
             pass
 with open('dictionaries/cleaned_senti', 'w') as j_file:
-    json.dump(new_dict,j_file, indent = 4, sort_keys = True, separators = (',',
-                                                                           ': '))
+    json.dump(new_dict, j_file, indent=4, sort_keys=True, separators=(',',
+                                                                      ': '))
